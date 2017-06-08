65daa6d2b0378c14b0c567824a40aab6112accd9
Learn <learn@learn14.local>
2017-06-08 12:00:40 -0700

    added Bevents table

--- bc-express/models/bevent.js
+++ bc-express/models/bevent.js
@@ -1,13 +1,20 @@
 'use strict';
 module.exports = function(sequelize, DataTypes) {
   var Bevent = sequelize.define('Bevent', {
-    date: DataTypes.DATE
+    date: DataTypes.DATE,
+    place_1_id: DataTypes.INTEGER,
+    place_2_id: DataTypes.INTEGER,
+    vote_status: DataTypes.BOOLEAN
   }, {
     classMethods: {
       associate: function(models) {
-        Bevent.belongsTo(models.Place, {
-          foreignKey: 'placeId',
-          as: 'Loc'
+        Bevent.hasMany(models.Place, {
+          foreignKey: 'place_1_id',
+          as: 'place_1'
+        })
+        Bevent.hasMany(models.Place, {
+          foreignKey: 'place_2_id',
+          as: 'place_2'
         })
       }
     }
