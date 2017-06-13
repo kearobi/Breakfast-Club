ec8a1b88f8d5ee33257becf4b2ced2a085d82cac
Learn <learn@learn14.local>
2017-06-13 10:13:17 -0700

    added yelp stars to EventDetail

--- bc-react/src/components/EventChoice.js
+++ bc-react/src/components/EventChoice.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react';
 
 class EventChoice extends Component {
 
-
   render() {
     return (
       <div>
         <p>Choice {this.props.choice}</p>
         <h4>{this.props.place.name}</h4>
         <img src={this.props.place.image_url} />
+        <img className="yelp-rating" src={`../Images/small_${this.props.place.yelp_rating}.png`} />
       </div>
     );
   }
--- bc-react/src/components/EventDetail.js
+++ bc-react/src/components/EventDetail.js
@@ -5,7 +5,20 @@ import RSVPButton from './RSVPButton';
 import EventChoice from './EventChoice'
 
 class EventDetail extends Component {
+  dateParser(){
+    var weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+    let temp = this.props.eventData.event.date.split('T')
+    let date = temp[0].split('-')
+    let dayOfWeek = weekday[new Date(date).getDay()]
+    let month = months[new Date(date).getMonth()]
+    let day = new Date(date).getDate()
+    let hourTime = new Date(temp).getHours()
+    let minuteTime = new Date(temp).getMinutes()
+    console.log(dayOfWeek, "," , month, "", day, " @ ", hourTime,":", minuteTime)
+  }
   render() {
+    this.dateParser()
     var mappedUsers;
     if (this.props.eventData.users.length == 0){
       mappedUsers = <p>No RSVPs yet</p>
@@ -21,6 +34,7 @@ class EventDetail extends Component {
       })
     }
 
+
     return (
       <div>
         <div>
@@ -31,6 +45,8 @@ class EventDetail extends Component {
           {(this.props.winner == 1 || this.props.winner == null) && <EventChoice
             place={this.props.eventData.places[0]}
             choice={1}
+
+
             />}
           {(this.props.winner == 2 || this.props.winner == null) && <EventChoice
             place={this.props.eventData.places[1]}
