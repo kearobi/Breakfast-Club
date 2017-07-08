19a7fd94a11b33d61343decc4efe8165edb39c22
Rachel <raquel.rudermano@gmail.com>
2017-07-07 18:42:48 -0700

    login fail if user's account is deactivated; created message for same

--- bc-express/app.js
+++ bc-express/app.js
@@ -431,7 +431,7 @@ app.post('/signup', function(request, response){
   })
 })
 
-app.put('/edit/user', authorization, function(request, response){
+app.put('/edit/user', function(request, response){
   console.log("request: ", request);
   console.log("response: ", response);
 
