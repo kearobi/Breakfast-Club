5d6b16536fcddd54d140950cbf6d8e7d695cdd7f
Rachel <raquel.rudermano@gmail.com>
2017-07-15 15:02:10 -0700

    created seed for messages

---
+++ bc-express/seeds/20170715215022-messages.js
@@ -0,0 +1,90 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    return queryInterface.bulkInsert('Messages', [
+      {
+        author: 'Rachel R.',
+        content: 'Welcome to Breakfast Club!',
+        user_id: 6,
+        createdAt: '2017-06-02 07:52:29-07',
+        updatedAt: '2017-06-02 07:52:29-07'
+      },
+      {
+        author: 'Nick B.',
+        content: "Raquel, where's breakfast at?",
+        user_id: 7,
+        createdAt: '2017-06-02 08:52:29-07',
+        updatedAt: '2017-06-02 08:52:29-07'
+      },
+      {
+        author: "Antonio N.",
+        content: "Don't worry about it",
+        user_id: 8,
+        createdAt: '2017-06-04 07:52:29-07',
+        updatedAt: '2017-06-04 07:52:29-07'
+      },
+      {
+        author: "BrazilGabe",
+        content: 'So...........',
+        user_id: 9,
+        createdAt: '2017-07-02 07:52:29-07',
+        updatedAt: '2017-07-02 07:52:29-07'
+      }
+    ])
+    .then(function(){
+      return queryInterface.sequelize.query('ALTER SEQUENCE "Places_id_seq" RESTART 10')
+    })
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+      Example:
+      return queryInterface.bulkInsert('Person', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+  },
+
+  down: function (queryInterface, Sequelize) {
+    return queryInterface.bulkDelete('Places', [
+      {
+        author: 'Rachel R.',
+        content: 'Welcome to Breakfast Club! Type your message here',
+        user_id: 6,
+        createdAt: '2017-06-02 07:52:29-07',
+        updatedAt: '2017-06-02 07:52:29-07'
+      },
+      {
+        author: 'Nick B.',
+        content: "Raquel, where's breakfast at?",
+        user_id: 7,
+        createdAt: '2017-06-02 08:52:29-07',
+        updatedAt: '2017-06-02 08:52:29-07'
+      },
+      {
+        author: "Antonio N.",
+        content: "Don't worry about it",
+        user_id: 8,
+        createdAt: '2017-06-04 07:52:29-07',
+        updatedAt: '2017-06-04 07:52:29-07'
+      },
+      {
+        author: "BrazilGabe",
+        content: 'So...........',
+        user_id: 9,
+        createdAt: '2017-07-02 07:52:29-07',
+        updatedAt: '2017-07-02 07:52:29-07'
+      }
+    ])
+    .then(function(){
+      return queryInterface.sequelize.query('ALTER SEQUENCE "Places_id_seq" RESTART 1')
+    })
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+      Example:
+      return queryInterface.bulkDelete('Person', null, {});
+    */
+  }
+};
