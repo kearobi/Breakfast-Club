171846ab83f18f7dc2b82475075d7251881833cb
Rachel Ruderman <raquel.rudermano@gmail.com>
2017-07-18 15:40:22 -0700

    created migration to add speaker column to Bevents table

---
+++ bc-express/migrations/20170718223755-add-speaker-column-to-Bevents.js
@@ -0,0 +1,23 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    return [
+      queryInterface.addColumn(
+        'Bevents',
+        'speaker',
+        {
+          type: Sequelize.STRING,
+          allowNull: true,
+          defaultValue: null
+        }
+      ),
+    ];
+  },
+
+  down: function (queryInterface, Sequelize) {
+    return [
+      queryInterface.removeColumn('Bevents', 'speaker'),
+    ];
+  }
+};
