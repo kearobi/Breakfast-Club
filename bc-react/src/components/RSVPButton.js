3d007d0a7bb2231b25d242217f3e36120d2b15fd
Rachel Ruderman <raquel.rudermano@gmail.com>
2017-07-19 15:46:12 -0700

    RSVP button styled and working

--- bc-react/src/components/RSVPButton.js
+++ bc-react/src/components/RSVPButton.js
@@ -24,10 +24,20 @@ class RSVPButton extends Component {
   }
 
   render() {
+    let yes
+    let no
+    if(this.state.user.rsvp){
+      yes='rsvp yes'
+      no='rsvp no'
+    } else{
+      no='rsvp yes'
+      yes='rsvp no'
+    }
+
     return (
-      <form className='flex-item'>
-        <input type="radio" name="rsvp" value={true} onClick={this.handleClick.bind(this)} /> Yes
-        <input type="radio" name="rsvp" value={false} onClick={this.handleClick.bind(this)} /> No
+      <form className = {yes}>
+        <button className={yes} type="button" name="rsvp" value={true} onClick={this.handleClick.bind(this)}>Yes </button>
+        <button className={no} type="button" name="rsvp" value={false} onClick={this.handleClick.bind(this)}>No </button>
       </form>
     );
   }
