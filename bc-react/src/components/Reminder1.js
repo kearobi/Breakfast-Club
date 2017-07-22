310016bcc5caf621547127d0d9d4994472201998
Rachel <raquel.rudermano@gmail.com>
2017-07-22 16:10:48 -0700

    working on reminder messages

--- bc-react/src/components/Reminder1.js
+++ bc-react/src/components/Reminder1.js
@@ -17,20 +17,18 @@ render() {
     let currentEvent = this.props.event;
     let user = this.props.user;
 
-    let weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-    let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
     let temp = currentEvent.event.date.split('T')
     let date = temp[0].split('-')
-    let dayOfWeek = weekday[new Date(date).getDay()]
-    let month = months[new Date(date).getMonth()]
+    // let month = months[new Date(date).getMonth()]
     let day = new Date(date).getDate()
     let hourTime = new Date(temp).getHours()
     let minuteTime = new Date(temp).getMinutes()
+    let weekday = currentEvent.event.date
 
     if (!currentEvent.event.vote_status){
       if (user.rsvp){
           message1 = `See you on `
-          link = `${dayOfWeek}`
+          // link = `${dayOfWeek}`
           //TODO = add time of event
           message2 = ` at ${currentEvent.event.winner === 1 ? currentEvent.places[0].name : currentEvent.places[1].name}!`
       }
@@ -42,7 +40,6 @@ render() {
     }
     else {
       if (user.voted){
-        let momentDate = this.props.event.event.date
         if (user.rsvp){
             greeting = `Hey ${user.firstName}, you're on the guest list!`
             message1 = 'The '
@@ -51,9 +48,12 @@ render() {
             message2 = function(){
 
               return(
+                <div>
+                  will be revealed&nbsp;
               <Moment format='dddd'>
-                {momentDate}
-              </Moment>
+                {weekday}
+              </Moment>&nbsp;at 12 pm
+              </div>
             )}
         }
         else {
@@ -64,7 +64,8 @@ render() {
       }
       else {
           //TODO = fix hardcorded "0 AM"
-          greeting = `Hey, ${user.firstName}! This ${dayOfWeek} at ${hourTime}:${minuteTime}0 AM`
+          greeting = `Hey, ${user.firstName}! This
+          at ${hourTime}:${minuteTime}0 AM`
           message1 = `${currentEvent.places[0].name} or ${currentEvent.places[1].name}? `
           link = "Cast your vote!"
           message2 = ""
@@ -78,9 +79,6 @@ render() {
         {message1}
         <Link to='/current-event'>{link}</Link>
         {message2()}
-        {/* <Moment format='dddd'>
-          {this.props.event.event.date}
-        </Moment> */}
       </div>
     );
   }
