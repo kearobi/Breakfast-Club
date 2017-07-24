035f7643bc29456d088988a80eb63f97f200ac19
Rachel Ruderman <raquel.rudermano@gmail.com>
2017-07-24 11:16:32 -0700

    changed past-event from POST to GET

--- bc-react/src/actions/EventActions.js
+++ bc-react/src/actions/EventActions.js
@@ -20,7 +20,7 @@ export function testCreate(){
 
 export function fetchPastEvent(id){
   const params = {
-    method: "POST",
+    method: "GET",
     headers: {'Content-Type': 'application/json'},
     body: JSON.stringify({
       id: id
