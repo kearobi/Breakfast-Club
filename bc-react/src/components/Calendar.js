be2c7c234d142c895fb2b9a0c1f8984127ad076a
Rachel Ruderman <raquel.rudermano@gmail.com>
2017-07-26 12:44:19 -0700

    created Calendar component

---
+++ bc-react/src/components/Calendar.js
@@ -0,0 +1,130 @@
+import React, {Component} from 'react';
+import {fetchEvents, fetchCurrentEvent} from '../actions/EventActions';
+import BigCalendar from 'react-big-calendar';
+import eventStore from '../stores/EventStore';
+import moment from 'moment';
+import {Redirect} from 'react-router-dom';
+import PastEvent from '../routes/PastEvent';
+import Modal from 'react-modal';
+
+BigCalendar.setLocalizer(
+  BigCalendar.momentLocalizer(moment)
+);
+
+const customStyle = {
+  overlay : {
+    position          : 'fixed',
+    top               : 0,
+    left              : 0,
+    right             : 0,
+    bottom            : 0,
+    backgroundColor   : 'rgba(255, 255, 255, 0.5)',
+    zIndex            : 5
+  },
+  content : {
+    top               : '50%',
+    left              : '50%',
+    right             : 'auto',
+    bottom            : 'auto',
+    marginRight       : '-50%',
+    transform         : 'translate(-50%, -50%)',
+    width             : '310px',
+    height            : '475px'
+  }
+};
+
+class Calendar extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      events: [],
+      selectedEventId: null,
+      modal: false
+    }
+    this.updateEvents = this.updateEvents.bind(this)
+    this.openModal = this.openModal.bind(this)
+    this.closeModal = this.closeModal.bind(this)
+      fetchEvents();
+  }
+
+  componentWillMount(){
+    eventStore.on('change', this.updateEvents)
+  }
+
+  componentWillUnmount(){
+    eventStore.removeListener('change', this.updateEvents)
+  }
+
+  updateEvents(){
+    let bevents = eventStore.getAllEvents()
+    console.log('all events: ', bevents)
+    let newEvents = bevents.map(function(bevent){
+      let start = moment(bevent.date).toDate()
+      let end = moment(bevent.date).add(1, 'hours').toDate()
+      let placeName = bevent.place.name
+      console.log('bevent', bevent)
+      let id = bevent.id
+      return {
+        'title': placeName,
+        'start': start,
+        'end': end,
+        'id': id,
+      }
+    })
+    this.setState({
+      events: newEvents
+    })
+  }
+
+  openModal(event, e){
+    this.setState({
+      selectedEventId: event.id,
+      modal: true
+    })
+  }
+
+  closeModal(){
+    this.setState({
+      selectedEventId: null,
+      modal: false
+    })
+  }
+
+  checkCalendar(){
+    if(this.state.events.length > 0){
+      return(
+      <BigCalendar
+        events={this.state.events}
+        onSelectEvent={this.openModal}
+      />
+    )
+  }else{
+    return(<div>Loading...</div>)
+    }
+  }
+
+  render(){
+    return (
+          <div>{/* //this is the flex container */}
+            <div className='polaroid'>
+              {(this.state.selectedEventId && this.state.selectedEventId === this.props.event.event.id) &&
+                <Redirect to='/current-event'/>
+              }
+              {(this.state.selectedEventId && this.state.selectedEventId !== this.props.event.event.id) &&
+              <Modal
+                isOpen={this.state.modal}
+                onRequestClose={this.closeModal}
+                style={customStyle}
+                contentLabel="Modal"
+              >
+              <PastEvent eventId={this.state.selectedEventId}/>
+            </Modal>
+              }
+            </div>
+            <div className="calendar-div">{this.checkCalendar()}</div>
+      </div>
+    );
+  }
+}
+
+export default Calendar;
--- bc-react/src/routes/Home.js
+++ bc-react/src/routes/Home.js
@@ -10,104 +10,54 @@ import Header from '../components/Header';
 import {Redirect} from 'react-router-dom';
 import PastEvent from './PastEvent';
 import Modal from 'react-modal';
-
-BigCalendar.setLocalizer(
-  BigCalendar.momentLocalizer(moment)
-);
-
-const customStyle = {
-  overlay : {
-    position          : 'fixed',
-    top               : 0,
-    left              : 0,
-    right             : 0,
-    bottom            : 0,
-    backgroundColor   : 'rgba(255, 255, 255, 0.5)',
-    zIndex            : 5
-  },
-  content : {
-    top               : '50%',
-    left              : '50%',
-    right             : 'auto',
-    bottom            : 'auto',
-    marginRight       : '-50%',
-    transform         : 'translate(-50%, -50%)',
-    width             : '310px',
-    height            : '475px'
-  }
-};
+import Calendar from '../components/Calendar'
+//
+// BigCalendar.setLocalizer(
+//   BigCalendar.momentLocalizer(moment)
+// );
+//
+// const customStyle = {
+//   overlay : {
+//     position          : 'fixed',
+//     top               : 0,
+//     left              : 0,
+//     right             : 0,
+//     bottom            : 0,
+//     backgroundColor   : 'rgba(255, 255, 255, 0.5)',
+//     zIndex            : 5
+//   },
+//   content : {
+//     top               : '50%',
+//     left              : '50%',
+//     right             : 'auto',
+//     bottom            : 'auto',
+//     marginRight       : '-50%',
+//     transform         : 'translate(-50%, -50%)',
+//     width             : '310px',
+//     height            : '475px'
+//   }
+// };
 
 class Home extends Component {
   constructor(props){
     super(props)
     this.state = {
-      events: [],
-      selectedEventId: null,
-      modal: false
+      // events: [],
     }
-    this.updateEvents = this.updateEvents.bind(this)
-    this.openModal = this.openModal.bind(this)
-    this.closeModal = this.closeModal.bind(this)
+    // this.updateEvents = this.updateEvents.bind(this)
       fetchCurrentEvent()
-      fetchEvents();
+      // fetchEvents();
   }
+  //
+  // componentWillMount(){
+  //   eventStore.on('change', this.updateEvents)
+  // }
+  //
+  // componentWillUnmount(){
+  //   eventStore.removeListener('change', this.updateEvents)
+  // }
+  //
 
-  componentWillMount(){
-    eventStore.on('change', this.updateEvents)
-  }
-
-  componentWillUnmount(){
-    eventStore.removeListener('change', this.updateEvents)
-  }
-
-  updateEvents(){
-    let bevents = eventStore.getAllEvents()
-    console.log('all events: ', bevents)
-    let newEvents = bevents.map(function(bevent){
-      let start = moment(bevent.date).toDate()
-      let end = moment(bevent.date).add(1, 'hours').toDate()
-      //where is place coming from?
-      let placeName = bevent.place.name
-      console.log('bevent', bevent)
-      let id = bevent.id
-      return {
-        'title': placeName,
-        'start': start,
-        'end': end,
-        'id': id,
-      }
-    })
-    this.setState({
-      events: newEvents
-    })
-  }
-
-  openModal(event, e){
-    this.setState({
-      selectedEventId: event.id,
-      modal: true
-    })
-  }
-
-  closeModal(){
-    this.setState({
-      selectedEventId: null,
-      modal: false
-    })
-  }
-
-  checkCalendar(){
-    if(this.state.events.length > 0){
-      return(
-      <BigCalendar
-        events={this.state.events}
-        onSelectEvent={this.openModal}
-      />
-    )
-  }else{
-    return(<div>Loading...</div>)
-    }
-  }
 
   render(){
     return (
@@ -120,23 +70,7 @@ class Home extends Component {
             <div className="welcome-message">
               <div className='reminder'><Reminder user={this.props.user} event={this.props.event}/></div>
             </div>
-            <div className='polaroid'>
-              {(this.state.selectedEventId && this.state.selectedEventId === this.props.event.event.id) &&
-                <Redirect to='/current-event'/>
-              }
-              {(this.state.selectedEventId && this.state.selectedEventId !== this.props.event.event.id) &&
-              <Modal
-                isOpen={this.state.modal}
-                onRequestClose={this.closeModal}
-                style={customStyle}
-                contentLabel="Modal"
-              >
-              <PastEvent eventId={this.state.selectedEventId}/>
-            </Modal>
-              }
-              {/* <img className='frame' src='../Images/polaroid.png' /> */}
-            </div>
-            <div className="calendar-div">{this.checkCalendar()}</div>
+            <Calendar event={this.props.event} />
         </div>
         </div>
         <img className='fruit-border' src='../Images/fruit-border.jpg' alt='fruit'></img>
