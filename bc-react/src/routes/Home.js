6e0d7803fcb04d2792f204b03d609fab59a47155
Rachel Ruderman <raquel.rudermano@gmail.com>
2017-07-26 13:50:04 -0700

    Home.js is stateless

--- bc-react/src/routes/Home.js
+++ bc-react/src/routes/Home.js
@@ -2,63 +2,11 @@ import React, {Component} from 'react';
 import SideBar from '../components/SideBar';
 import SideBarMini from '../components/SideBarMini';
 import Reminder from '../components/Reminder';
-import {fetchEvents, fetchCurrentEvent} from '../actions/EventActions';
-import BigCalendar from 'react-big-calendar';
-import eventStore from '../stores/EventStore';
-import moment from 'moment';
 import Header from '../components/Header';
 import {Redirect} from 'react-router-dom';
-import PastEvent from './PastEvent';
-import Modal from 'react-modal';
 import Calendar from '../components/Calendar'
-//
-// BigCalendar.setLocalizer(
-//   BigCalendar.momentLocalizer(moment)
-// );
-//
-// const customStyle = {
-//   overlay : {
-//     position          : 'fixed',
-//     top               : 0,
-//     left              : 0,
-//     right             : 0,
-//     bottom            : 0,
-//     backgroundColor   : 'rgba(255, 255, 255, 0.5)',
-//     zIndex            : 5
-//   },
-//   content : {
-//     top               : '50%',
-//     left              : '50%',
-//     right             : 'auto',
-//     bottom            : 'auto',
-//     marginRight       : '-50%',
-//     transform         : 'translate(-50%, -50%)',
-//     width             : '310px',
-//     height            : '475px'
-//   }
-// };
 
 class Home extends Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      // events: [],
-    }
-    // this.updateEvents = this.updateEvents.bind(this)
-      fetchCurrentEvent()
-      // fetchEvents();
-  }
-  //
-  // componentWillMount(){
-  //   eventStore.on('change', this.updateEvents)
-  // }
-  //
-  // componentWillUnmount(){
-  //   eventStore.removeListener('change', this.updateEvents)
-  // }
-  //
-
-
   render(){
     return (
           <div className="wrapper">{/* //this is the flex container */}
